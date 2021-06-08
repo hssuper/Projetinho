2dc976a0384b3acd693aa3f49e820f84306a9f53
hssuper <higorsouza2306@gmail.com>
2021-06-08 09:01:55 -0300

    R$

--- js/operadoresAritmeticos.js
+++ js/operadoresAritmeticos.js
@@ -1,12 +1,14 @@
 var subtotal = (13+1)*5;
 var frete = 0.5 * (13+1);
 var total = subtotal + frete;
+var real = 'R$: ';
+
 
 var subResultado = document.getElementById('subtotalvalor');
-subResultado.textContent = 'R$: ' + subtotal;
+subResultado.textContent += real + subtotal;
 
 var freteResultado = document.getElementById( 'fretevalor');
-freteResultado.textContent = 'R$: ' + frete;
+freteResultado.textContent += real + frete;
 
 var totalResultado = document.getElementById('totalvalor');
-totalResultado.textContent = 'R$: ' + total;
+totalResultado.textContent += real + total;
