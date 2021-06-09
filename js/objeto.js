ddd87ab6ce9aeaa924b4961c52fde63d778a60da
hssuper <higorsouza2306@gmail.com>
2021-06-09 09:41:03 -0300

    d9ah

--- js/objeto.js
+++ js/objeto.js
@@ -1,14 +1,27 @@
 var hotel = {
-    totalQuartos: 70,
+    totalQuartos: 100,
     Reservados: 30,
-    ChecaDisponibilidade: function () {
-        return this.totalQuartos - this.Reservados;
+    disponibilidade: function () {
+        return this.totalQuartos, this.Reservados;
+
+
+
     }
-    }
 
+}
 
+var totalQuartos = document.getElementById('disponibilidade');
+totalQuartos = 100;
 
+document.getElementById('disponibilidade').click(function () {
+    totalQuartos--;
+});
+var Reservados = document.getElementById('disponibilidade');
+Reservados = 30;
 
+document.getElementById('disponibilidade').click(function () {
+    Reservados--;
+});
 
 var totalQuartos = document.getElementById('totalQuartos');
 totalQuartos.textContent = hotel.totalQuartos;
@@ -16,6 +29,5 @@ totalQuartos.textContent = hotel.totalQuartos;
 var Reservados = document.getElementById('reservados');
 Reservados.textContent = hotel.Reservados;
 
-var disponibilidade = document.getElementById('disponibilidade')
-disponibilidade.textContent = hotel.ChecaDisponibilidade ;
+
 
